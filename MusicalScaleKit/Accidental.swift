e0a7ce6b333a965a26d87f13095c5dc939ed0da6
26hrs <106030732+26hrs@users.noreply.github.com>
2020-11-21 16:36:57 +0000

    Added project files.

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -3,4 +3,4 @@
 // Copyright (c) 2015 muukii
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associa
+// of this software and associated document
