a465d51931c8f41b88f270ed4911ce2c1e172031
26hrs <106030732+26hrs@users.noreply.github.com>
2020-11-21 06:21:04 +0000

    Work on documentation

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -2,4 +2,4 @@
 //
 // Copyright (c) 2015 muukii
 //
-// Permission is hereby granted, free of charge, to any person obta
+// Permission is hereby granted, free of charge, to any person obtaining a copy
