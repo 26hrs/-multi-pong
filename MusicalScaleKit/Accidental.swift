5543334991cb853429055f5419917b7739ef25fc
26hrs <106030732+26hrs@users.noreply.github.com>
2020-11-23 09:47:10 +0000

    update timestamp

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -3,4 +3,4 @@
 // Copyright (c) 2015 muukii
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated document
+// of this software and associated documentation files (the
