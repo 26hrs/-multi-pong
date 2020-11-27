b6df0aed873376ce8a18b9b8a572cf65413cf773
26hrs <106030732+26hrs@users.noreply.github.com>
2020-11-27 08:52:19 +0000

    add some tests

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -3,4 +3,5 @@
 // Copyright (c) 2015 muukii
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to dea
+// of this software and associated documentation files (the "Software"), to deal
+// in the Softw
