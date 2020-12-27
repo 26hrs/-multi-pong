be1ba91c04c24d74f811ca3593fec841c05d948e
26hrs <106030732+26hrs@users.noreply.github.com>
2020-12-27 16:37:48 +0000

    Delete main

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -6,4 +6,4 @@
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copie
+// copies of the Sof
