985f8d67ffb8214861e266a04ac243cbaa53532c
26hrs <106030732+26hrs@users.noreply.github.com>
2021-02-28 22:13:33 +0000

    Removing debug code

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -12,4 +12,4 @@
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PR
+// THE SOFTWARE IS PROVIDED "AS I
