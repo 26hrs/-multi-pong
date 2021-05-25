98d2c372d3850af4316a3840f3a998797321ed49
26hrs <106030732+26hrs@users.noreply.github.com>
2021-05-25 09:51:58 +0000

    fixed #7

--- MusicalScaleKit/Accidental.swift
+++ MusicalScaleKit/Accidental.swift
@@ -20,4 +20,4 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-public enum Accidental: CustomStringConvertible, CustomDebugStringConverti
+public enum Accidental: CustomStringConvertible, CustomDebugStringConvertible {
