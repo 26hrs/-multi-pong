c15f0723ec9c69b3df443df2211ea7c857c08f3a
26hrs <106030732+26hrs@users.noreply.github.com>
2022-09-28 18:46:13 +0000

    clean up repo

--- MusicalScaleKitTests/MusicalScaleKitTests.swift
+++ MusicalScaleKitTests/MusicalScaleKitTests.swift
@@ -12,3 +12,15 @@ class MusicalScaleTests: XCTestCase {
 
   override func tearDown() {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testNoteAngle() {
+    XCTAssertEqual(Note.a(.natural).sharp().circleAngle, Note.b(.flat).circleAngle)
+    XCTAssertEqual(Note.b(.natural).sharp().circleAngle, Note.c(.natural).circleAngle)
+    XCTAssertEqual(Note.a(.natural).flat().circleAngle, Note.g(.sharp).circleAngle)
+  }
+
+  func testsharp() {
+
+    do { // A
